2b81e08eb44766782a3c2e0340446e1c6eedebbf
Bot85 <ankitmishra301302303@gmail.com>
2026-02-21 21:09:17 +0530

    adding backend code

--- aviServer/index.js
+++ aviServer/index.js
@@ -1,1 +1,10 @@
-console.log("server is start");
+import express from "express"
+const app=express();
+const port=5173;
+
+app.get("/",(req, res)=>{
+res.send("<h1>hello ankit ji</h1>");
+});
+app.listen(port,()=>{
+    console.log(`sever ${port} is running` );
+})
